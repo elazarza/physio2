1f9cf8ec6cc7a5f3b1d4c625ba00b927fafa39a3
elazarza <elazarzadiki@gmail.com>
2019-03-15 09:48:12 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.parent').hover(function () {
+        $('.float-menu').fadeIn();
+    }); 
+
+    $('.parent')(function () {
+        $('.float-menu').fadeOut();
+    });
+});
+
+margin-right: 0;
+    margin-left: 5px;
+    height:100%;
+    transition: .3s ease;
+    padding: 3px 15px 3px 15px;
